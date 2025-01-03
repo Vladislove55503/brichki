e197c78646a438379f852264e3e9c4c344b3a4a8
vladislove2003 <vladwgm@gmail.com>
2025-01-03 16:36:09 +0500

    Созданы модели БД для приложения FILTER.

--- brichki/FILTER/models.py
+++ brichki/FILTER/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Brand(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+class Model(models.Model):
+    brand = models.ForeignKey('Brand', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+
+class Generation(models.Model):
+    model = models.ForeignKey('Model', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
