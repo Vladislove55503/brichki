623e77ea5daccf0941156e186b6a971af4e4096f
vladislove2003 <vladwgm@gmail.com>
2025-01-13 20:41:28 +0500

    Новый шаблон для главной страницы (Фильтр). Не получается в шаблоне сделать, чтобы значение из БД (одна цифра (0-10)) отображалось, как 'Седан'/'Хэтчбэк' и т.д. В модели прописан класс с константами, но логично, что отображается только цифра. Уверен решение можно найти в базовых шаблонах django, потому что в админке при записи объекта этой модели, список отображается не цифрами, а соответствующими словами.

--- brichki/ADS/views.py
+++ brichki/ADS/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+from ADS.models import Ads
+
 
 def index(request):
     context = {
         'title_page': 'Brichki.ru',
+        'title_center': 'filter',
+        'title_right': 'catalog',
     }
-    return render(request, 'brichki/base.html', context)
+    return render(request, 'ADS/main.html', context)
