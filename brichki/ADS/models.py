4ff43fc73e0fb2ba4a211c451b94d2c4e93b3fbd
vladislove2003 <vladwgm@gmail.com>
2025-01-16 13:57:34 +0500

    Проблема из двух предыдущих коммитов решена - созданы новые таблицы (Body, EngineType, BoostType, Drive, Broken) для параметров машины. В таблице Ads уже были значения, и начинались они с нуля (0), поэтому чтобы они сошлись с id новых таблиц пришлось каждое значение увеличить на 1. Теперь всё сходится.

--- brichki/ADS/models.py
+++ brichki/ADS/models.py
@@ -1,52 +1,36 @@
 from django.db import models
-from django.db.models import SET_NULL
 
 from FILTER.models import Brand
 from FILTER.models import Model
 from FILTER.models import Generation
 
-class Parameters(models.Model):
+
+class Body(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    value = models.IntegerField(unique=True)
 
     def __str__(self):
         return self.name
 
-class Body(Parameters, models.Model):
-    pass
-
-
-# class Body(models.Model):
-#     name = models.CharField(max_length=100, unique=True)
-#     value = models.IntegerField(unique=True)
-#
-#     def __str__(self):
-#         return self.name
-
 class EngineType(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    value = models.IntegerField(unique=True)
 
     def __str__(self):
         return self.name
 
 class BoostType(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    value = models.IntegerField(unique=True)
 
     def __str__(self):
         return self.name
 
 class Drive(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    value = models.IntegerField(unique=True)
 
     def __str__(self):
         return self.name
 
 class Broken(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    value = models.IntegerField(unique=True)
 
     def __str__(self):
         return self.name
@@ -57,15 +41,14 @@ class Ads(models.Model):
     model = models.ForeignKey(Model, on_delete=models.SET_NULL, null=True, verbose_name='Модель')
     generation = models.ForeignKey(Generation, on_delete=models.SET_NULL, null=True, verbose_name='Поколение')
 
-    body = models.ForeignKey('Body', on_delete=SET_NULL, null=True, verbose_name='Кузов')
+    body = models.ForeignKey(Body, on_delete=models.SET_NULL, null=True, verbose_name='Кузов')
 
-    engine_type = models.ForeignKey('EngineType', on_delete=SET_NULL, null=True, verbose_name='Двигатель')
-    boost_type = models.ForeignKey('BoostType', on_delete=SET_NULL, null=True, verbose_name='Наддув')
+    engine_type = models.ForeignKey(EngineType, on_delete=models.SET_NULL, null=True, verbose_name='Двигатель')
+    boost_type = models.ForeignKey(BoostType, on_delete=models.SET_NULL, null=True, verbose_name='Наддув')
+
     engine_capacity = models.FloatField(verbose_name='Объём двигателя')
-
-    drive = models.ForeignKey('Drive', on_delete=SET_NULL, null=True, verbose_name='Привод')
-    broken = models.ForeignKey('Broken', on_delete=SET_NULL, null=True, verbose_name='Битая')
-
+    drive = models.ForeignKey(Drive, on_delete=models.SET_NULL, null=True, verbose_name='Привод')
+    broken = models.ForeignKey(Broken, on_delete=models.SET_NULL, null=True, verbose_name='Битая')
     mileage = models.IntegerField(default=0, verbose_name='Пробег')
     price = models.IntegerField(verbose_name='Цена')
     comment = models.CharField(max_length=10000, blank=True, default='', verbose_name='Комментарий')
