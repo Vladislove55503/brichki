bf75aadb1c25c61c8cad6587964f814ab0ec3525
vladislove2003 <vladwgm@gmail.com>
2025-01-27 12:28:08 +0500

    Механика фильтра готова, всё работает. Теперь нужно сверстать страницу объявления и страницу каталога.

--- brichki/ADS/views.py
+++ brichki/ADS/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import render
 
 from ADS.models import Ads
@@ -14,13 +15,45 @@ from FILTER.models import Broken
 def index(request):
     print(f'INDEX - {request.POST}')
 
+    if request.POST:
+        print('POST')
+        filter_parameter = {
+            'brand_id': request.POST['brand'],
+            'model_id': request.POST['model'],
+            'generation_id': request.POST['generation'],
+            'engine_type_id': request.POST['engine'],
+            'boost_type_id': request.POST['boost'],
+            'engine_capacity__gte': request.POST['capcty'],
+            'engine_capacity__lte': request.POST['capcty-before'],
+            'mileage__gte': request.POST['mileage'],
+            'mileage__lte': request.POST['mileage-before'],
+            'price__gte': request.POST['price'],
+            'price__lte': request.POST['price-before'],
+            'drive_id': request.POST['drive'],
+            'body_id': request.POST['body'],
+        }
+
+        filters = Q()
+        for key, value in filter_parameter.items():
+            if value:
+                filters &= Q(**{key: value})
+        print(filters)
+
+        ads = Ads.objects.filter(filters)
+    else:
+        ads = Ads.objects.all()
+
     context = {
         'title_page': 'Brichki.ru',
         'title_center': 'filter',
         'title_right': 'catalog',
 
-        'Ads': Ads.objects.all(),
+        'Ads': ads,
+
         'Brand': Brand.objects.all(),
+        'Model': Model.objects.all(),
+        'Generation': Generation.objects.all(),
+
         'Body': Body.objects.all(),
         'EngineType': EngineType.objects.all(),
         'BoostType': BoostType.objects.all(),
